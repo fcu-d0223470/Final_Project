5f5bef804353dc2d0a25612598d59776d5f4d4ba
jacky <jackykokzk@gmail.com>
2017-04-11 19:07:48 +0800

    layout update and java

---
+++ app/src/main/java/com/example/user/project/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.user.project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView tv1 = (TextView) findViewById(R.id.main_tv);
+    }
+}
