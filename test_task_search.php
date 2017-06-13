5813ba4243d1f486f3877d7a28e352f3a64d3c48
FH12\user <bobo940928@gmail.com>
2017-06-13 19:16:27 +0800

    01

--- test_task_search.php
+++
@@ -1,6 +0,0 @@
-請輸入文字以便使用POST傳送：<br />
-<form method="POST" action="task_search.php">
-<H6>搜尋</H6><input type="text" name="char" size="20" value="" /><br />
-<input type="hidden" name="op" value="showValue" />
-<input name="Submit1" type="submit" value="送出" /><br />
-</form>
