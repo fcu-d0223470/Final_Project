2a3a539aa8f677796a4417d79c9387f9e546be3d
fcu-d0239795 <a0936782861@gmail.com>
2017-06-13 18:05:25 +0800

    APP後端測試用

---
+++ test_forget.php
@@ -0,0 +1,8 @@
+請輸入文字以便使用POST傳送：<br />
+<form method="POST" action="user_forget.php">
+<H6>帳號</H6><input type="text" name="account" size="20" value="" /><br />
+<input type="hidden" name="op" value="showValue" />
+<H6>E-Mail</H6><input type="text" name="email" size="40" value="" /><br />
+<input type="hidden" name="op" value="showValue" />
+<input name="Submit1" type="submit" value="送出" /><br />
+</form>
---
+++ test_register.php
@@ -0,0 +1,22 @@
+﻿請輸入文字以便使用POST傳送：<br />
+<form method="POST" action="http://www.ebizlearning.com.my/web/phone/user_register.php">
+<H6>帳號</H6><input type="text" name="account" size="20" value="" /><br />
+<input type="hidden" name="op" value="showValue" />
+<H6>密碼</H6><input type="text" name="password" size="20" value="" /><br />
+<input type="hidden" name="op" value="showValue" />
+<H6>姓</H6><input type="text" name="fname" size="10" value="" /><br />
+<input type="hidden" name="op" value="showValue" />
+<H6>名</H6><input type="text" name="lname" size="20" value="" /><br />
+<input type="hidden" name="op" value="showValue" />
+<H6>性別</H6><input type="text" name="sex" size="5" value="" /><br />
+<input type="hidden" name="op" value="showValue" />
+<H6>電話</H6><input type="text" name="phone" size="10" value="" /><br />
+<input type="hidden" name="op" value="showValue" />
+<H6>E-Mail</H6><input type="text" name="email" size="40" value="" /><br />
+<input type="hidden" name="op" value="showValue" />
+<H6>地址</H6><input type="text" name="address" size="40" value="" /><br />
+<input type="hidden" name="op" value="showValue" />
+<H6>手機編號</H6><input type="text" name="phone_number" size="40" value="" /><br />
+<input type="hidden" name="op" value="showValue" />
+<input name="Submit1" type="submit" value="送出" /><br />
+</form>
