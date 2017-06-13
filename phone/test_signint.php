0a9d26bef44bdefb0e14668fa130d7d71b0d0286
jim3878 <jim3878@gmail.com>
2017-06-13 19:26:46 +0800

    1

---
+++ phone/test_signint.php
@@ -0,0 +1,10 @@
+﻿請輸入文字以便使用POST傳送：<br />
+<form method="POST" action="http://www.ebizlearning.com.my/web/phone/user_signint.php">
+<H6>帳號</H6><input type="text" name="account" size="20" value="" /><br />
+<input type="hidden" name="op" value="showValue" />
+<H6>密碼</H6><input type="text" name="password" size="20" value="" /><br />
+<input type="hidden" name="op" value="showValue" />
+<H6>手機型號</H6><input type="text" name="phone_number" size="10" value="" /><br />
+<input type="hidden" name="op" value="showValue" />
+<input name="Submit1" type="submit" value="送出" /><br />
+</form>
---
+++ phone/test_update_flag.php
@@ -0,0 +1,16 @@
+請輸入文字以便使用POST傳送：<br />
+<form method="POST" action="update_flag.php">
+<H6>id</H6><input type="text" name="have_id" size="20" value="" /><br />
+<input type="hidden" name="op" value="showValue" />
+<H6>任務編號</H6><input type="text" name="have_number" size="10" value="" /><br />
+<input type="hidden" name="op" value="showValue" />
+<H6>Hflag</H6><input type="text" name="have_flag" size="5" value="" /><br />
+<input type="hidden" name="op" value="showValue" />
+<H6>Tflag</H6><input type="text" name="have_task_flag" size="5" value="" /><br />
+<input type="hidden" name="op" value="showValue" />
+<H6>Sflag</H6><input type="text" name="have_story_flag" size="5" value="" /><br />
+<input type="hidden" name="op" value="showValue" />
+<H6>Spart</H6><input type="text" name="story_part" size="5" value="" /><br />
+<input type="hidden" name="op" value="showValue" />
+<input name="Submit1" type="submit" value="送出" /><br />
+</form>
