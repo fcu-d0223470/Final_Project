4c3a75630e130db7b7fd20f04f63b8499df9920c
d0280464 <bobo83928@yahoo.com.tw>
2017-06-13 19:22:16 +0800

    0

--- test_change_phone.php
+++
@@ -1,6 +0,0 @@
-請輸入文字以便使用POST傳送：<br />
-<form method="POST" action="change_phone.php">
-<H6>ID</H6><input type="text" name="id" size="20" value="" /><br />
-<input type="hidden" name="op" value="showValue" />
-<input name="Submit1" type="submit" value="送出" /><br />
-</form>
